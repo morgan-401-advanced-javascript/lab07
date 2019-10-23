b9670bbf2ce4a6bf22fcb32341317ad5c2c0be12
morgantatums <morgantatums@zillowgroup.com>
2019-10-22 17:14:15 -0700

    added teams route

--- routes/teams-routes.js
+++ routes/teams-routes.js
@@ -10,3 +10,22 @@ router.get('/', (req, res, next) => {
   let results = db.teams;
   res.json({ count, results });
 });
+
+router.get('/:id', (req, res, next) => {
+  let id = req.params.id;
+  let record = db.teams.filter(record => record.id === parseInt(id));
+  res.json(record[0]);
+});
+
+router.delete('/:id', (req, res, next) => {
+  let id = req.params.id;
+  let record = db.teams.filter(record => record.id === parseInt(id));
+  let index = db.teams.indexOf(record);
+  let results = db.teams;
+  results.slice(index);
+  let count = db.teams.length;
+  res.json({ count, results });
+    
+});
+
+module.exports = router;
